d0a02ea4f165da0672441275c652d8c2bc06d36f
Dmitriy <xs_ds@inbox.ru>
2023-06-05 11:39:27 +0500

    add tu_uppercase function and capitalize_words

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def to_uppercase(line):
+    """Функция возвращает результат в виде строки, состоящей из слов в верхнем регистре"""
     return line.upper()
+
+
+def capitalize_words(string):
+    """"Функция возвращает результат в виде строки, состоящей из слов с заглавными первыми буквами"""
+    return ' '.join([word.capitalize() for word in string.split()])
